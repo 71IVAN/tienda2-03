a1dbb81ac1548e68619e524c30cdc7d1b61a84ce
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-02-15 08:45:04 -0500

    voy a ganar este taller

---
+++ src/controllers/controlador_home.js
@@ -0,0 +1,10 @@
+//Definiendo datos
+//let variable locañ
+//var variable global
+
+
+let nombre = "James rodriguez" //global
+if(5<2){
+    let nombre = "Lucho diaz" //local
+}
+console.log("hola "+nombre)
